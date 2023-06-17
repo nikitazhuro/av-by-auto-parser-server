3b5ed941eb803cf05d52db3c8ab0885f226f1933
Nikita Zhuro <nikitazhuro@yandex.by>
2023-06-17 21:20:52 +0300

    add filter

--- src/mileage-cars/dto/mileage-cars.dto.ts
+++ src/mileage-cars/dto/mileage-cars.dto.ts
@@ -1,9 +1,27 @@
+export class Filter {
+  readonly price_from: string;
+  readonly price_to: string;
+  readonly mileage_from: string;
+  readonly mileage_to: string;
+  readonly engine_from: string;
+  readonly engine_to: string;
+  readonly engine_type: string[];
+  readonly transmission_type: string[];
+  readonly body_type: string;
+  readonly drive_type: string;
+  readonly interior_material: string;
+  readonly color: string;
+  readonly year_from: string;
+  readonly year_to: string;
+}
+
 export class GetMileageCars {
   readonly brand: string;
   readonly model: string;
   readonly generation: string;
   readonly engine_capacity?: string;
   readonly year?: string;
+  readonly filter?: Filter;
 }
 
 export class DeleteCar {
--- src/mileage-cars/mileage-cars.service.ts
+++ src/mileage-cars/mileage-cars.service.ts
@@ -1,8 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
 
 import { DeleteCar, GetMileageCars } from './dto/mileage-cars.dto';
 import { MileageCarsModel } from './mileage-cars.model';
+import { isEmpty } from 'lodash';
 
 @Injectable()
 export class MileageCarsService {
@@ -43,31 +45,111 @@ export class MileageCarsService {
   }
 
   async getAll(getMileageCars: GetMileageCars) {
-    if (getMileageCars.year) {
-      return this.mileageCarsRepository.findAll({
-        where: {
-          year: getMileageCars.year,
-          customIds: {
-            avby: {
-              brandsId: getMileageCars.brand,
-              modelId: getMileageCars.model,
-              generationId: getMileageCars.generation,
-            },
+    const { filter, brand, model, generation, year } = getMileageCars;
+
+    const whereConfig: any = {
+      customIds: {
+        avby: {
+          brandsId: brand,
+          modelId: model,
+          generationId: generation,
+        },
+      },
+      data: {
+        properties: {
+          mileage_km: {
+            value: {},
+          },
+          engine_capacity: {
+            value: {},
+          },
+          engine_type: {
+            value: {},
+          },
+          transmission_type: {},
+          body_type: {
+            value: {},
+          },
+          drive_type: {
+            value: {},
           },
         },
-      });
-    }
-
-    return this.mileageCarsRepository.findAll({
-      where: {
-        customIds: {
-          avby: {
-            brandsId: getMileageCars.brand,
-            modelId: getMileageCars.model,
-            generationId: getMileageCars.generation,
+        price: {
+          usd: {
+            amount: {},
           },
         },
       },
+    };
+
+    if (filter) {
+      if (filter.year_from && !filter.year_to) {
+        whereConfig.year = {
+          [Op.gte]: +filter.year_from,
+        };
+      }
+      if (!filter.year_from && filter.year_to) {
+        whereConfig.year = {
+          [Op.lte]: +filter.year_to,
+        };
+      }
+      if (filter.year_from && filter.year_to) {
+        whereConfig.year = {
+          [Op.gte]: +filter.year_from,
+          [Op.lte]: +filter.year_to,
+        };
+      }
+
+      if (filter.engine_type) {
+        whereConfig.data.properties.engine_type.value[Op.in] =
+          filter.engine_type;
+      }
+
+      if (filter.transmission_type) {
+        whereConfig.data.properties.transmission_type.value =
+          filter.transmission_type;
+      }
+
+      if (filter.body_type) {
+        whereConfig.data.properties.body_type.value[Op.in] = filter.body_type;
+      }
+
+      if (filter.drive_type) {
+        whereConfig.data.properties.drive_type.value[Op.in] = filter.drive_type;
+      }
+
+      if (filter.price_from) {
+        whereConfig.data.price.usd.amount[Op.gte] = +filter.price_from;
+      }
+      if (filter.price_to) {
+        whereConfig.data.price.usd.amount[Op.lte] = +filter.price_to;
+      }
+
+      if (filter.mileage_from) {
+        whereConfig.data.properties.mileage_km.value[Op.gte] =
+          +filter.mileage_from;
+      }
+      if (filter.mileage_to) {
+        whereConfig.data.properties.mileage_km.value[Op.lte] =
+          +filter.mileage_to;
+      }
+
+      if (filter.engine_from) {
+        whereConfig.data.properties.engine_capacity.value[Op.gte] =
+          filter.engine_from;
+      }
+      if (filter.engine_to) {
+        whereConfig.data.properties.engine_capacity.value[Op.lte] =
+          filter.engine_to;
+      }
+    }
+
+    if (!isEmpty(whereConfig.year) && year) {
+      whereConfig.year = year;
+    }
+
+    return this.mileageCarsRepository.findAll({
+      where: whereConfig,
     });
   }
 
